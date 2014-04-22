58cbe9f2bc7e19f2a457b688af9fd01d0ef515aa
Jan <krimdomu@googlemail.com>
2014-04-22 12:54:26 +0200

    initial commit

---
+++ __module__.pm
@@ -0,0 +1,58 @@
+#
+# AUTHOR: jan gehring <jan.gehring@gmail.com>
+# REQUIRES:
+# LICENSE: Apache License 2.0
+#
+# Simple Module to install Apache on your Server.
+
+package Rex::NTP::Base;
+
+use Rex -base;
+use Rex::Ext::ParamLookup;
+
+our %package_map = (
+  debian => "ntp",
+  ubuntu => "ntp",
+  centos => "ntp",
+  redhat => "ntp",
+);
+
+our %service_name_map = (
+  debian => "ntp",
+  ubuntu => "ntp",
+  centos => "ntpd",
+  redhat => "ntpd",
+);
+
+our %configuration_file_map = (
+  debian => "/etc/ntp.conf",
+  ubuntu => "/etc/ntp.conf",
+  centos => "/etc/ntp.conf",
+  redhat => "/etc/ntp.conf",
+);
+
+task setup => make {
+  my $pkg = param_lookup "package_name",
+    $package_map{ lc get_operating_system };
+
+  my $service = param_lookup "service_name",
+    $service_name_map{ lc get_operating_system };
+
+  my $configuration_file = param_lookup "configuration_file",
+    $configuration_file_map{ lc get_operating_system };
+
+  my $ntp_server = param_lookup "ntp_server", ["pool.ntp.org"];
+
+  pkg $pkg, ensure => "latest";
+
+  file $configuration_file,
+    content   => template("templates/etc/ntp.conf.tpl"),
+    owner     => "root",
+    group     => "root",
+    mode      => 644,
+    on_change => make { notify service => $service; };
+
+  service $service, ensure => "started";
+};
+
+1;
